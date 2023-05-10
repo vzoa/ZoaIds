9c17817c24fe7f2159ffbeb0d4a78c9ffe58041b
Ken Greim <34892440+kengreim@users.noreply.github.com>
2023-05-09 21:21:09 -0700

    Removing old src folder with refactor

--- src/Server/Data/VatsimSnapshot.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ZoaIds.Server.Data
-{
-    public class VatsimSnapshot
-    {
-        public long Id { get; set; }
-        public DateTime Time { get; set; }
-        public string RawJson { get; set; }
-    }
-}
--- src/Server/Services/IRouteSummaryService.cs
+++
@@ -1,8 +0,0 @@
-﻿using ZoaIds.Shared.Models;
-
-namespace ZoaIds.Server.Services;
-
-public interface IRouteSummaryService
-{
-    public Task<RealWorldRouting> FetchRoutesAsync(string departureIcao, string arrivalIcao);
-}
